eadb0e80172a913e0e6f5d0947380aad48e49102
Umut <140441119+xHector1337@users.noreply.github.com>
2024-08-19 13:29:49 +0300

    Update videodownloader.py

--- videodownloader.py
+++ videodownloader.py
@@ -1,34 +1,73 @@
 from pytube import YouTube
 from os import getcwd
-def MP4LowestResolution(url):
+import pathlib
+import argparse
+def MP4LowestResolution(url,path="None"):
     try:
         yt = YouTube(url)
         stream = yt.streams.filter(file_extension="mp4").get_lowest_resolution()
-        stream.download(output_path=getcwd())
+        if path == "None":
+            stream.download(output_path=getcwd())
+        else:
+            stream.download(output_path=path)    
     except Exception as e:
         if e == "get_throttling_function_name: could not find match for multiple":
             print(f"{e} error! Check https://github.com/pytube/pytube/issues/1954 for solution!")
         else:    
             print(f"Error: {e}")    
 MP4LowestResolution("https://youtu.be/ss6lhSwb-u8")
-def MP4HighestResolution(url):
+def MP4HighestResolution(url,path="None"):
     try:
         yt = YouTube(url)
         stream = yt.streams.filter(file_extension="mp4").get_highest_resolution()
-        stream.download(output_path=getcwd())
+        if path == "None":
+            stream.download(output_path=getcwd())
+        else:
+            stream.download(output_path=path)    
     except Exception as e:
         if e == "get_throttling_function_name: could not find match for multiple":
             print(f"{e} error! Check https://github.com/pytube/pytube/issues/1954 for solution!")
         else:
             print(f"Error {e}")
-def MP4OnlyAudio(url):
+def MP3(url,path="None"):
     try:
         yt = YouTube(url)
         stream = yt.streams.get_audio_only()
-        stream.download(output_path=getcwd())
+        ohmygotto = ""
+        if path == "None":
+            stream.download(output_path=getcwd())
+            ohmygotto = pathlib.Path(stream.get_file_path())
+        else:
+            stream.download(output_path=path)       
+            ohmygotto = pathlib.Path(f"{path}{yt.title}.mp4")
+        ohmygotto.rename(ohmygotto.with_suffix(".mp3"))
     except Exception as e:
         if e == "get_throttling_function_name: could not find match for multiple":
             print(f"{e} error! Check https://github.com/pytube/pytube/issues/1954 for solution!")
         else:
             print(f"Error {e}")
-MP4OnlyAudio("https://www.youtube.com/watch?v=YW_ucgRrW6w")                                                
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url",help="Url of video to be downloaded.")
+    parser.add_argument("filetype",help="Specify file type as mp4 file.(mp4/mp3)")
+    parser.add_argument("--lowestresolution",help="Specify video resolution as the lowest.",action="store_true")
+    parser.add_argument("--path",help="Specify download path. It uses current working directory as default.")
+    args = parser.parse_args()
+    if args.url and "https" and ("youtube.com" or "youtu.be") in args.url:
+        if args.filetype == "mp4" and args.path and not args.lowestresolution:
+            MP4HighestResolution(args.url,args.path)
+        elif args.filetype == "mp4" and args.path and args.lowestresolution:
+            MP4LowestResolution(args.url,args.path)
+        elif args.filetype == "mp4" and not args.path and not args.lowestresolution:
+            MP4HighestResolution(args.url)
+        elif args.filetype == "mp3" and not args.path:
+            MP3(args.url)
+        elif args.filetype == "mp3" and args.path:
+            MP3(args.url,args.path)
+        else:
+            print("Unsupported method found.")
+    else:
+        print("Enter an YouTube url.")
+main()                                             
+        
